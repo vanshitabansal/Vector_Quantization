c278478423fca7edea85ab438cdf1d332d51880c
vanshita bansal <59884554+vanshitabansal@users.noreply.github.com>
2021-09-26 11:25:33 +0530

    Update Kmeans.cpp

--- KMeans/Kmeans/Kmeans.cpp
+++ KMeans/Kmeans/Kmeans.cpp
@@ -11,23 +11,22 @@
 #define p 12
 #define k 8
 #define M 6340
-FILE *file_input, *file_output,*ref,*test,*normalised;
+FILE *file_input, *file_output;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("This is Assignment 4 (214101056) \n");
-	printf("Please wait..The processing is going on! \n");
+	printf("This is Vector Quantization using KMeans or LLoyd's Algorithm  (214101056) \n");
 	printf("---------------------------------------------------------------------------------\n");
-	//Declaring all the required arrays and variables
 
+	//Declaring all the required arrays and variables
 	char arr[1024],*line,ch;
-	long double universe[M][p]={{0}},codebook[k][p]={{0}},num=0,dist=0,distortion=DBL_MAX,avg_dist=0,delta=0.00001;
+	long double universe[M][p]={{0}},codebook[k][p]={{0}},num=0,dist=0,distortion=100000000.0,avg_dist=0,delta=0.00001;
 	double tokhura_weights[]={1.0,3.0,7.0,13.0,19.0,22.0,25.0,33.0,42.0,50.0,56.0,61.0};
 	int cnt=0,index=0,row=0,col=0;
 	
 	file_input = fopen("Universe.csv","r"); //open input file in read mode
 	
-	//till end of fileis not reached
+	//Reading the csv file and writing data in a 2D array.
 	while(fgets(arr, 1024, file_input))
 	{
 		line=strtok(arr,",");
@@ -41,33 +40,40 @@ int _tmain(int argc, _TCHAR* argv[])
 		
 	}
 	
-	for(int i=0;i<6340;i++){
-		for(int j=0;j<12;j++){
-			printf("%lf ",universe[i][j]);
-		}
-		printf("\n ");
-	}
-	printf("---------------------------------------------------------\n ");
+	printf("Universe array generated!\n\n");
+
 	initialize_codebook(universe,codebook);
+
+	printf("The initialised codebook : \n\n");
 	for(int i=0;i<k;i++){
 		for(int j=0;j<p;j++){
 			printf("%lf ",codebook[i][j]);
 		}
-		printf("\n ");
+		printf("\n");
 	}
-	int count=0;
+	printf("---------------------------------------------------------------------------------\n");
+
+	int count=0; //number of iterations
 	while(true){
 		count++;
-		dist=kmeans(universe,codebook,tokhura_weights);		
+		dist=kmeans(universe,codebook,tokhura_weights);	 //finding distortion using kmeans algorithm	
 		avg_dist=dist*1.0/M;
-		printf("Average Distortion:%lf %lf %d\n",distortion,avg_dist,count);
-		if(abs(distortion-avg_dist)>delta)			
+		
+		if(abs(distortion-avg_dist)>delta)				//if difference between current and previous distortion is less than delta stop		
 			distortion=avg_dist;
 		else
 			break;
 
 	}
-	printf("Average Distortion: %lf %d\n",avg_dist,count);
+	printf("The final codebook : \n\n");
+	for(int i=0;i<k;i++){
+		for(int j=0;j<p;j++){
+			printf("%lf ",codebook[i][j]);
+		}
+		printf("\n");
+	}
+	printf("---------------------------------------------------------------------------------\n");
+	printf("Average Distortion using Kmeans: %lf \n",avg_dist);
 	return 0;
 }
 
