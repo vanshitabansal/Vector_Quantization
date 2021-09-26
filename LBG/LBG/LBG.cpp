9d64ead23283f232d60307b232389dc5c260c2ee
vanshita bansal <59884554+vanshitabansal@users.noreply.github.com>
2021-09-26 12:11:55 +0530

    Add files via upload

---
+++ LBG/LBG/LBG.cpp
@@ -0,0 +1,101 @@
+// LBG.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "ctype.h"
+#include<stdlib.h>
+#include "util.h"    //this is the header file containing all the functions used while prediction
+#include <math.h>
+#include<float.h>
+#include<string.h>
+#define p 12
+#define k 8
+#define M 6340
+FILE *file_input, *file_output;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("This is Vector Quantization using LBG Algorithm (214101056) \n");
+	printf("\n-----------------------------------------------------------------------------------------------------------------------\n\n");
+	//Declaring all the required arrays and variables
+
+	char arr[1024],*line,ch;
+	long double universe[M][p]={{0}},codebook[k][p]={{0}},num=0,dist=0,distortion=DBL_MAX,avg_dist=0,delta=0.00001,epsilon=0.03;
+	double tokhura_weights[]={1.0,3.0,7.0,13.0,19.0,22.0,25.0,33.0,42.0,50.0,56.0,61.0};
+	int cnt=0,index=0,row=0,col=0,m=0;
+	
+	file_input = fopen("Universe.csv","r"); //open input file in read mode
+	
+	//Reading the csv file and writing data in a 2D array.
+	while(fgets(arr, 1024, file_input))
+	{
+		line=strtok(arr,",");
+		col=0;
+		while(line!=NULL){
+			universe[row][col]=atof(line);
+			++col;
+			line=strtok(NULL,",");
+		}
+		row++;
+		
+	}
+
+	printf("Universe array generated!\n\n");
+
+	initialize_codebook(universe,codebook);
+
+	printf("The initialised codebook : \n\n");
+	for(int i=0;i<k;i++){
+		for(int j=0;j<p;j++){
+			printf("%lf ",codebook[i][j]);
+		}
+		printf("\n");
+	}
+	printf("\n-----------------------------------------------------------------------------------------------------------------------\n\n");
+
+	int count=0; //number of iterations
+	index=0,m=1;
+	long double y_new1[p]={0},y_new2[p]={0};
+	while(m<k){
+		distortion=10000000.0;
+		count++;
+		for(int ind=0;ind<m;ind++){
+			for(int i=0;i<p;i++){
+				y_new1[i]=codebook[ind][i]+epsilon;
+			}
+			for(int i=0;i<p;i++){
+				y_new2[i]=codebook[ind][i]-epsilon;
+			}
+			for(int i=0;i<p;i++){
+				codebook[ind][i]=y_new1[i];
+				if(ind+m<k) codebook[ind+m][i]=y_new2[i];
+			}			
+		}
+		m=2*m;
+		while(true){
+		
+				dist=kmeans(universe,codebook,tokhura_weights,m);		 //finding distortion using kmeans algorithm
+				avg_dist=dist*1.0/M;
+				if(abs(distortion-avg_dist)>delta)						//if difference between current and previous distortion is less than delta stop		
+					distortion=avg_dist;
+				else
+					break;
+
+		}
+		printf("Codebook for m = %d :\n\n",m);
+		for(int i=0;i<k;i++){
+			for(int j=0;j<p;j++){
+				printf("%lf ",codebook[i][j]);
+			}
+			printf("\n");
+		}
+		printf("\n-----------------------------------------------------------------------------------------------------------------------\n\n");
+		
+	}
+	printf("Average Distortion using LBG: %lf \n",avg_dist);
+	printf("\n-----------------------------------------------------------------------------------------------------------------------\n\n");
+	return 0;
+}
+
+
---
+++ LBG/LBG/util.h
@@ -0,0 +1,84 @@
+// util.h : include file for project specific files
+//that are used frequently, but are changed infrequently
+#include "stdafx.h"
+#include "stdio.h"
+#include "ctype.h"
+#include<stdlib.h>
+#include<math.h>
+#include<float.h>
+#include<time.h>
+#define p 12
+#define k 8
+#define N 320
+#define M 6340
+void initialize_codebook(long double x[M][p],long double y[][p]){
+	long double sum[p];
+
+	//initialising codebook with centroid of universe
+	for(int j=0;j<p;j++){
+		sum[j]=0;
+		for(int i=0;i<M;i++){		
+			sum[j]+=x[i][j];
+		}
+	}
+	for(int j=0;j<p;j++){
+		y[0][j]=sum[j]/M;
+	}
+}
+
+long double kmeans(long double x[M][p],long double y[k][p],double tokhura_weights[p],int m){
+	
+	long double min_dist=DBL_MAX,temp_dist=0,dist=0;
+	int bucket[k]={0},pos=0,region[k][M];
+	int distances[k]={0};
+	//Classify
+	for(int i=0;i<M;i++){ 
+		min_dist=1000000000.0;
+		for(int j=0;j<m;j++){
+			distances[j]=0;
+			for(int u=0;u<p;u++){
+				//finding tokhura's distance between universe vector and codebook vector
+				distances[j]+=tokhura_weights[u]*(x[i][u]-y[j][u])*(x[i][u]-y[j][u]);
+			}
+
+			
+		}	
+		//finding minimum distance vector in codebook and saving its index
+		for(int j=0;j<m;j++){
+			if(min_dist>distances[j]){
+				min_dist=distances[j];
+				pos=j;
+			}
+		}
+		
+		//Assigning the universe vector to codebook region
+		region[pos][bucket[pos]++]=i;
+
+		//Total Distortion
+		dist+=min_dist;
+		
+	}
+
+	//Centroid updation
+	for(int i=0;i<m;i++){	
+		long double centroid[p]={0};
+
+		//Finding sum of Ci's to get centroid of a region
+		for(int u=0;u<p;u++){
+			for(int j=0;j<bucket[i];j++){
+				 centroid[u]+=x[region[i][j]][u];
+			}
+		}
+
+		//Finding the centroid of region
+		for(int u=0;u<p;u++){
+			centroid[u]/=bucket[i];
+		}
+
+		//Updating codebook with new centroid
+		for(int u=0;u<p;u++){
+			y[i][u]=centroid[u];
+		}
+	}
+	return dist;
+}
